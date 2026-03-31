9d1cb00a57a829c8d8bd9362e92a1a51e35eb14e
mshemanth10 <mshemanth15112003@gmail.com>
2026-04-01 01:14:06 +0530

    fixed database for render

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expense_db',
-        'USER': 'expense_user',
-        'PASSWORD': 'Hemanth@1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
